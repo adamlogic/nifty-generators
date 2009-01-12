85eac97611c851f547cda630a44e0784bca24e46
Adam McCrea <adam@adamlogic.com>
2009-01-12 12:43:33 -0500

    Always use HAML and compact CSS

--- rails_generators/nifty_layout/nifty_layout_generator.rb
+++ rails_generators/nifty_layout/nifty_layout_generator.rb
@@ -10,14 +10,8 @@ class NiftyLayoutGenerator < Rails::Generator::Base
       m.directory 'public/stylesheets'
       m.directory 'app/helpers'
       
-      if options[:haml]
-        m.directory 'public/stylesheets/sass'
-        m.template "layout.html.haml", "app/views/layouts/#{file_name}.html.haml"
-        m.file     "stylesheet.sass",  "public/stylesheets/sass/#{file_name}.sass"
-      else
-        m.template "layout.html.erb", "app/views/layouts/#{file_name}.html.erb"
-        m.file     "stylesheet.css",  "public/stylesheets/#{file_name}.css"
-      end
+      m.template "layout.html.haml", "app/views/layouts/#{file_name}.html.haml"
+      m.file "stylesheet.css",  "public/stylesheets/#{file_name}.css"
       m.file "helper.rb", "app/helpers/layout_helper.rb"
     end
   end
@@ -31,7 +25,6 @@ class NiftyLayoutGenerator < Rails::Generator::Base
     def add_options!(opt)
       opt.separator ''
       opt.separator 'Options:'
-      opt.on("--haml", "Generate HAML for view, and SASS for stylesheet.") { |v| options[:haml] = v }
     end
 
     def banner
